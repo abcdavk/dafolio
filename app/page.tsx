801581311792cb60ebd03f6a46fe75cffbf29654
abcdavk <xloot.mcpedl@gmail.com>
2024-12-27 11:30:58 +0700

    Edit content

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,15 @@ import Link from "next/link";
 import { SiFiverr } from "react-icons/si";
 import { SocialLinks } from "./components/footer";
 
+import { formatDate, getBlogPosts } from "app/lib/posts";
+
 export default function Page() {
+  let allBlogs = getBlogPosts();
+
+  // Sort blogs by date (newest first)
+  const latestBlog = allBlogs
+    .sort((a, b) => new Date(b.metadata.publishedAt) - new Date(a.metadata.publishedAt))
+    .slice(0, 1);
   return (
     <section>
       
@@ -49,7 +57,7 @@ export default function Page() {
           Thank you for visiting. Let’s build something amazing together!
         </p>
 
-        <hr className="border-t-2 border-prose-neutral w-full my-4" />
+        <hr className="border-t-2 border-slate-500/50 w-full my-4" />
 
         <b>Freelance Services at Your Fingertips</b>
 
@@ -64,7 +72,7 @@ export default function Page() {
         <p>
           Let’s collaborate and bring your projects to life. Explore my portfolio or connect with me to discuss your ideas—I’m always open to new challenges!
         </p>
-        <div className="p-6 h-[800px]">
+        <div className="p-6 h-[600px]">
           <iframe
             src={socialLinks.fiverr}
             className="w-full h-full rounded-lg"
@@ -72,6 +80,45 @@ export default function Page() {
           ></iframe>
         </div>
         
+      </div>
+
+      <hr className="border-t-2 border-slate-500/50 w-full my-4" />
+
+      <div>
+        <h1 className="mb-8 text-2xl font-medium tracking-tight">Read Latest Our Blog</h1>
+        <div>
+          {latestBlog.map((post) => (
+            <Link
+              key={post.slug}
+              className="flex flex-col space-y-1 mb-4 transition-opacity duration-200 hover:opacity-80 p-4 rounded-md bg-blue-500/5 outline outline-1 outline-blue-500/20"
+              href={`/blog/${post.slug}`}
+            >
+              <div className="w-full flex flex-col sm:flex-row justify-between items-start sm:items-center">
+                {/* Title and Summary */}
+                <div className="flex-1 hover:underline">
+                  <p className="text-black dark:text-white tracking-tight">
+                    {post.metadata.title}
+                  </p>
+                  <p className="hover:underline text-neutral-600 dark:text-neutral-400 tabular-nums text-sm">
+                    {post.metadata.summary}
+                  </p>
+                </div>
+                {/* Date */}
+                <p className="text-neutral-600 dark:text-neutral-400 tabular-nums text-sm sm:ml-4">
+                  {formatDate(post.metadata.publishedAt, false)}
+                </p>
+              </div>
+            </Link>
+          ))}
+          <Link href='/blog'>
+            <div className="flex justify-center items-center transition-opacity duration-200 hover:opacity-80 py-4 w-full rounded-md bg-blue-500/5 outline outline-1 outline-blue-500/40">
+              <p className="text-black dark:text-white hover:underline">
+                Read more
+              </p>
+            </div>
+          </Link>
+
+        </div>
       </div>
     </section>
   );
