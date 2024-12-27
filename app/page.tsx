d9d86792ab670948475cac6e15d5340b0547ebfd
abcdavk <xloot.mcpedl@gmail.com>
2024-12-27 12:26:43 +0700

    Fix code error

--- app/page.tsx
+++ app/page.tsx
@@ -92,7 +92,7 @@ export default function Page() {
               key={post.slug}
               className="flex flex-col space-y-1 mb-4 transition-opacity duration-200 hover:opacity-80 p-4 rounded-md bg-blue-500/5 outline outline-1 outline-blue-500/20"
               href={`/blog/${post.slug}`}
-            >
+            > 
               <div className="w-full flex flex-col sm:flex-row justify-between items-start sm:items-center">
                 {/* Title and Summary */}
                 <div className="flex-1 hover:underline">
