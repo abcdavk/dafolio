011835fdf00d880051f58550b1ccfec5fe367a2b
abcdavk <xloot.mcpedl@gmail.com>
2026-08-12 20:10:30 +0700

    feat: add vue-final-modal

--- src/Art.vue
+++ src/Art.vue
@@ -1,11 +1,45 @@
+<script setup lang="ts">
+import PopImage from './components/PopImage.vue'
+</script>
+
 <template>
-  <div class="art-container center">
+  <div class="center">
     <h1>Art</h1>
+  </div>
+  <div class="art-container">
+    <Vue3Marquee :duration="32" :pause-on-hover="true" class="art-track">
+      <PopImage src="/img/arts/photo1.png" />
+      <PopImage src="/img/arts/photo2.png" />
+      <PopImage src="/img/arts/profile.png" />
+      <PopImage src="/img/arts/photo1.png" />
+      <PopImage src="/img/arts/photo2.png" />
+      <PopImage src="/img/arts/profile.png" />
+      <PopImage src="/img/arts/photo1.png" />
+      <PopImage src="/img/arts/photo2.png" />
+      <PopImage src="/img/arts/profile.png" />
+    </Vue3Marquee>
+    <Vue3Marquee :duration="32" :pause-on-hover="true" direction="reverse" class="art-track">
+      <PopImage src="/img/arts/photo1.png" />
+      <PopImage src="/img/arts/photo2.png" />
+      <PopImage src="/img/arts/profile.png" />
+      <PopImage src="/img/arts/photo1.png" />
+      <PopImage src="/img/arts/photo2.png" />
+      <PopImage src="/img/arts/profile.png" />
+      <PopImage src="/img/arts/photo1.png" />
+      <PopImage src="/img/arts/photo2.png" />
+      <PopImage src="/img/arts/profile.png" />
+    </Vue3Marquee>
   </div>
 </template>
 
 <style>
 .art-container {
-  min-height: 60vh;
+  min-height: 80vh;
+  overflow: hidden;
+}
+
+.art-track {
+  overflow: hidden;
+  position: relative;
 }
 </style>
